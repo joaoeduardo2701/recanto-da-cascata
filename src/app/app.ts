a73c3accbc7a3f4e95ddc92f4bf7fb4dc7ee576a
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-01-22 00:16:52 -0300

    section footer adicionada

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
 import { Sobre } from "./components/sobre/sobre";
 import { Galeria } from "./components/galeria/galeria";
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, Hero, Sobre, Galeria],
+  imports: [NavbarComponent, Hero, Sobre, Galeria, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
